20bcbbfced00e7d41a0a026bfd617e2bf66abed1
Сергей Мусаэлян <Musaelyan.sergei@gmail.com>
2022-05-22 18:52:12 +0300

    Project Start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  // state = {
+  //   currentPage: "home"
+  // };
+
+  // navigateTo = (page) => { 
+  //   this.setState({currentPage:page});
+  // }
+  
+  render() { 
+    return <>App</>;
+  }
+}
+
+export default App;
