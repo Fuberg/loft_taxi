54a2ac4f4abdc555ed5090a71a2c536601f6124a
Сергей Мусаэлян <Musaelyan.sergei@gmail.com>
2022-05-30 14:51:27 +0300

    Готово ДЗ 1-ой недели

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import { Home } from './Home';
+import { About } from './About';
+import { Profile } from './Profile';
 import './App.css';
 
-class App extends React.Component {
-  // state = {
-  //   currentPage: "home"
-  // };
+const PAGES = {
+  home: <Home/>,
+  about: <About />,
+  profile: <Profile/>
+}
 
-  // navigateTo = (page) => { 
-  //   this.setState({currentPage:page});
-  // }
+class App extends React.Component {
+  state = {
+    currentPage: "home"
+  };
+
+  navigateTo = (page) => { 
+    this.setState({currentPage:page});
+  }
   
   render() { 
-    return <>App</>;
+    return <>
+      <header class="topline">
+        <nav class="menu">
+          <ul class="menu__list">
+            <li class="menu__item">
+              <button class="menu__button" onClick={() => this.navigateTo("home")}>Home</button>
+            </li>
+            <li class="menu__item">
+              <button class="menu__button" onClick={() => this.navigateTo("about")}>About</button>
+            </li>
+            <li class="menu__item">
+              <button class="menu__button" onClick={() => this.navigateTo("profile")}>Profile</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main class="main">
+        <section class="section">
+          {PAGES[this.state.currentPage]}
+        </section>
+      </main>
+    </>;
   }
 }
 
